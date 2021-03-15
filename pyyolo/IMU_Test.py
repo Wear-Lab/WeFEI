ba223696621919388dd637505e7657f52fcd1c65
Eli Smith <elijah.matthew.smith@gmail.com>
2021-03-15 13:18:03 -0400

    Working on object positioning

--- pyyolo/IMU_Test.py
+++ pyyolo/IMU_Test.py
@@ -1,7 +1,16 @@
 import pyrealsense2 as rs
 import numpy as np
 import time
- 
+import math
+
+theta = [0.0] * 3
+alpha = 0.98
+firstGyro = True
+firstAccel = True
+lastGyroTimestamp = 0
+
+# FIX THETA, READ NOTES PAGE
+
 def initialize_camera():
 	# start the frames pipe
 	p = rs.pipeline()
@@ -11,96 +20,69 @@ def initialize_camera():
 	prof = p.start(conf)
 	return p
 
-
-def gyro_data(gyro):
+def gyroToArray(gyro):
 	return np.asarray([gyro.x, gyro.y, gyro.z])
 
+def processGyro(gyro, ts):
+	global theta, alpha, firstGyro, lastGyroTimestamp
+	
+	if firstGyro:
+		firstGyro = False
+		lastGyroTimestamp = ts
+		return
+	gyroAngle = [0.0] * 3
+	gyroAngle[0] = gyro.x
+	gyroAngle[1] = gyro.y
+	gyroAngle[2] = gyro.z
 
-def accel_data(accel):
+	tsDiff = (ts - lastGyroTimestamp) / 1000.0
+	lastGyroTimestamp = ts
+
+	gyroAngle[0] *= tsDiff
+	gyroAngle[1] *= tsDiff
+	gyroAngle[2] *= tsDiff
+
+	theta[0] += -1 * gyroAngle[2]
+	theta[1] += -1 * gyroAngle[1]
+	theta[2] += gyroAngle[0]
+
+def accelToArray(accel):
 	return np.asarray([accel.x, accel.y, accel.z])
 
+def processAccel(accel):
+	global theta, alpha, firstAccel, lastGyroTimestamp
+
+	accelAngle = [0.0] * 3
+
+	accelAngle[2] = math.atan2(accel.y, accel.z)
+	accelAngle[0] = math.atan2(accel.x, math.sqrt(accel.y * accel.y + accel.z * accel.z)) 
+
+	if firstAccel:
+		firstAccel = False
+		theta = accelAngle
+		theta[1] = math.pi
+	else:
+		theta[0] = theta[0] * alpha + accelAngle[0] * (1 - alpha)
+		theta[2] = theta[2] * alpha + accelAngle[2] * (1 - alpha)
+
+# MAIN
 p = initialize_camera()
 try:
 	while True:
 		f = p.wait_for_frames()
-		accel = accel_data(f[0].as_motion_frame().get_motion_data())
-		gyro = gyro_data(f[1].as_motion_frame().get_motion_data())
-		print("accelerometer: ", accel)
-		print("gyro: ", gyro)
+		accel = f[0].as_motion_frame().get_motion_data()
+		gyro = f[1].as_motion_frame().get_motion_data()
+		
+		processAccel(accel)
+		processGyro(gyro, time.time())
+
+		print("accelerometer: ", accelToArray(accel))
+		print("gyro: ", gyroToArray(gyro))
+		print("theta: " + str(theta))
 		time.sleep(0.5)
 finally:
 	p.stop()
 
 
+# THIS AIN'T WORKING
 
-
-'''if (rs2::pose_frame pose_frame = frameset.first_or_default(RS2_STREAM_POSE))
-{
-rs2_pose pose_sample = pose_frame.get_pose_data();
-std::cout << "Pose:" << pose_sample.translation.x << ", " << pose_sample.translation.y << ", " << pose_sample.translation.z << std::endl;
-#...
-}'''
-
-
-'''
-
-void process_gyro(rs2_vector gyro_data, double ts)
-{
-    if (first) // On the first iteration, use only data from accelerometer to set the camera's initial position
-    {
-        last_ts_gyro = ts;
-        return;
-    }
-    // Holds the change in angle, as calculated from gyro
-    float3 gyro_angle;
-
-     // Initialize gyro_angle with data from gyro
-    gyro_angle.x = gyro_data.x; // Pitch
-    gyro_angle.y = gyro_data.y; // Yaw
-    gyro_angle.z = gyro_data.z; // Roll
-
-    // Compute the difference between arrival times of previous and current gyro frames
-    double dt_gyro = (ts - last_ts_gyro) / 1000.0;
-    last_ts_gyro = ts;
-
-    // Change in angle equals gyro measurements * time passed since last measurement
-    gyro_angle = gyro_angle * dt_gyro;
-
-    // Apply the calculated change of angle to the current angle (theta)
-    std::lock_guard<std::mutex> lock(theta_mtx);
-    theta.add(-gyro_angle.z, -gyro_angle.y, gyro_angle.x);
-}
-
-void process_accel(rs2_vector accel_data)
-{
-    // Holds the angle as calculated from accelerometer data
-    float3 accel_angle;
-    // Calculate rotation angle from accelerometer data
-    accel_angle.z = atan2(accel_data.y, accel_data.z);
-    accel_angle.x = atan2(accel_data.x, sqrt(accel_data.y * accel_data.y + accel_data.z * accel_data.z));
-    // If it is the first iteration, set initial pose of camera according to accelerometer data
-    // (note the different handling for Y axis)
-    std::lock_guard<std::mutex> lock(theta_mtx);
-    if (first)
-    {
-        first = false;
-        theta = accel_angle;
-        // Since we can't infer the angle around Y axis using accelerometer data, we'll use PI as a convetion
-        // for the initial pose
-        theta.y = PI;
-    }
-    else
-    {
-        /* 
-        Apply Complementary Filter:
-            - "high-pass filter" = theta * alpha:  allows short-duration signals to pass through while
-              filtering out signals that are steady over time, is used to cancel out drift.
-            - "low-pass filter" = accel * (1- alpha): lets through long term changes, filtering out short
-              term fluctuations 
-        */
-        theta.x = theta.x * alpha + accel_angle.x * (1 - alpha);
-        theta.z = theta.z * alpha + accel_angle.z * (1 - alpha);
-    }
-}
-
-'''
--- pyyolo/runDepthTest.sh
+++
@@ -1,3 +0,0 @@
-export LIB_DARKNET=/home/wearlabucf/Desktop/WeFEI/YOLOv4/libdarknet.so
-export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python3.6/pyrealsense2
-python3 depthTest.py
